09f86a6f9c4bb9d3504d6b5ccf2d4cfe57be7290
Harshit Poddar <poddarharshit.2000@gmail.com>
2023-01-11 21:46:05 +0530

    Till loginform

--- src/Components/loginform.jsx
+++ src/Components/loginform.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Input from "./Common/input";
-import Joi from 'joi-browser'
+import Joi from "joi-browser";
+import { result } from "lodash";
 
 class LoginForm extends Component {
   state = {
@@ -9,22 +10,20 @@ class LoginForm extends Component {
   };
 
   schema = {
-    username: Joi.string().required(),
-    password: Joi.string().required()
-  }
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
+  };
 
   validate = () => {
-    Joi.validate(this.state.account,schema)
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.account, this.schema, options);
 
+    if (!error) return null;
     const errors = {};
 
-    const { account } = this.state;
-    if (account.username.trim() === "")
-      errors.username = "Username is required";
-    if (account.password.trim() === "")
-      errors.password = "Password is required";
+    for (let item of error.details) errors[item.path[0]] = item.message;
 
-    return Object.keys(errors).length === 0 ? null : errors;
+    return errors;
   };
   handleSubmit = (e) => {
     e.preventDefault();
@@ -36,14 +35,12 @@ class LoginForm extends Component {
     //call server
     console.log("Submitted");
   };
-  validateProperty = ({name,value}) => {
-    if (name === 'username'){
-        if (value.trim() === '') return 'Username is required'
-    if (name === 'password'){
-        if (value.trim() === '') return 'Password is required'
-    }
-  }
-}
+  validateProperty = ({ name, value }) => {
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
+  };
   handleChange = ({ currentTarget: input }) => {
     const errors = { ...this.state.errors };
     const errorMessage = this.validateProperty(input);
@@ -76,11 +73,13 @@ class LoginForm extends Component {
             error={errors.password}
           />
 
-          <button className="btn btn-primary">Login</button>
+          <button disabled={this.validate()} className="btn btn-primary">
+            Login
+          </button>
         </form>
       </div>
     );
   }
 }
 
-export default LoginForm
+export default LoginForm;
